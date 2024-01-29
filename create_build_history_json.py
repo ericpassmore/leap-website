cdefe704741f44908bf352e13ee76d06a45e7d46
Eric Passmore <eric.passmore@gmail.com>
2024-01-29 11:23:30 -0800

    add release notes

--- create_build_history_json.py
+++ create_build_history_json.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     parser.add_argument('--full-checksum', type=str, help='checksum')
     parser.add_argument('--pr-number', type=str, help='pr-number')
     parser.add_argument('--title', type=str, help='title')
+    parser.add_argument('--release-notes', type=str, help='title')
     parser.add_argument('--download-url', type=str, help='download url')
     parser.add_argument('--deb-file-name', type=str, help='deb file name')
     parser.add_argument('--file', type=str, help='file to write into')
@@ -22,20 +23,34 @@ if __name__ == '__main__':
     # Get the last 5 characters
     sum_last_5 = args.full_checksum[-5:]
     abbr_checksum = sum_first_5 + "..." + sum_last_5
-    # init data structure 
+    # init data structure
     data = []
 
-    record = {
-        "merge-time": args.merge_time,
-        "branch": args.branch,
-        "git-short-sha": args.git_short_sha,
-        "full-checksum": args.full_checksum,
-        "abbr-checksum": abbr_checksum,
-        "pr-number": args.pr_number,
-        "title": args.title,
-        "download-url": args.download_url,
-        "deb-file-name": args.deb_file_name,
-    }
+    if ! args.release_notes:
+        record = {
+            "merge-time": args.merge_time,
+            "branch": args.branch,
+            "git-short-sha": args.git_short_sha,
+            "full-checksum": args.full_checksum,
+            "abbr-checksum": abbr_checksum,
+            "pr-number": args.pr_number,
+            "title": args.title,
+            "download-url": args.download_url,
+            "deb-file-name": args.deb_file_name,
+        }
+    else
+        record = {
+            "merge-time": args.merge_time,
+            "branch": args.branch,
+            "git-short-sha": args.git_short_sha,
+            "full-checksum": args.full_checksum,
+            "abbr-checksum": abbr_checksum,
+            "pr-number": args.pr_number,
+            "title": args.title,
+            "release-notes": args.release_notes,
+            "download-url": args.download_url,
+            "deb-file-name": args.deb_file_name,
+        }
 
     if os.path.exists(args.file):
         with open(args.file, 'r') as build_history_json:
